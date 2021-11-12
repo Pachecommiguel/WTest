61ac798e122b4335bad597870feb8605baa59d53
Miguel Pacheco de Matos <miguel.matos@wit-software.com>
2021-11-12 17:48:48 +0000

    Add MainViewModel.kt and configure HiltApp

---
+++ app/src/main/java/com/example/wtest/ui/activities/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.wtest.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.wtest.R
+import com.example.wtest.ui.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
