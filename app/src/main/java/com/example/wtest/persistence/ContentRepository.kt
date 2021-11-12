087dbcf2e6a9461a33b58807c05c810e064ae44a
Miguel Pacheco de Matos <miguel.matos@wit-software.com>
2021-11-12 18:10:42 +0000

    Add contentRepository

---
+++ app/src/main/java/com/example/wtest/persistence/ContentRepository.kt
@@ -0,0 +1,10 @@
+package com.example.wtest.persistence
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ContentRepository @Inject constructor() {
+
+
+}
--- app/src/main/java/com/example/wtest/ui/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/wtest/ui/viewmodels/MainViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.wtest.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.example.wtest.persistence.ContentRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel() {
+class MainViewModel @Inject constructor(
+    repository: ContentRepository
+) : ViewModel() {
 
 }
